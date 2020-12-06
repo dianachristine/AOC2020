07592d1b0e19e7754239dad02618b0aa64ff37d5
ditarr <ditarr@ttu.ee>
2020-12-06 18:45:34 +0200

    day06

---
+++ day06/day06.py
@@ -0,0 +1,40 @@
+from data import get_data_from_fail
+
+
+def get_yes_count_of_one_group(group_answers: str) -> int:
+    counted_answers = set()
+    for answer in group_answers:
+        if answer.isalpha():
+            counted_answers.add(answer)
+    return len(counted_answers)
+
+
+def is_yes_in_all_answers(question, answers):
+    for answer in answers:
+        if not question.isalpha() or question not in answer:
+            return False
+    return True
+
+
+def get_all_yes_count_of_one_group(group_answers: str) -> int:
+    counted_answers = set()
+    answers = group_answers.split("\n")
+    if answers:
+        for question in answers[0]:
+            if is_yes_in_all_answers(question, answers):
+                counted_answers.add(question)
+    return len(counted_answers)
+
+
+def get_yes_count_of_all_groups(groups_answers: list, all_yes: bool) -> int:
+    yes_count = 0
+    for answers in groups_answers:
+        yes_count += get_all_yes_count_of_one_group(answers) if all_yes else get_yes_count_of_one_group(answers)
+    return yes_count
+
+
+if __name__ == '__main__':
+    data = get_data_from_fail("data.txt", "\n\n")
+
+    print(get_yes_count_of_all_groups(data, False))  # 6885
+    print(get_yes_count_of_all_groups(data, True))  # 3550
